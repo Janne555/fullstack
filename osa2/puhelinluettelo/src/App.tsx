1f719683eeed3ba072519ee5a6ec1204bb1b2b53
Janne Lavila <janne.lavila@wheelq.com>
2019-10-13 17:26:50 +0300

    implement 2.8

--- osa2/puhelinluettelo/src/App.tsx
+++ osa2/puhelinluettelo/src/App.tsx
@@ -1,16 +1,18 @@
 import React, { useState } from 'react'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+  const [persons, setPersons] = useState<{ name: string, number: string }[]>([
+    { name: 'Arto Hellas', number: '123123123' }
   ])
-  const [newName, setNewName] = useState('')
+  const [newName, setNewName] = useState<string>('')
+  const [newNumber, setNewNumber] = useState<string>('')
 
   function handleSubmit(e: React.FormEvent) {
     e.preventDefault()
     if (persons.every(person => person.name !== newName)) {
-      setPersons(prev => [...prev, { name: newName }])
+      setPersons(prev => [...prev, { name: newName, number: newNumber }])
       setNewName('')
+      setNewNumber('')
     }
     else
       alert(`${newName} is already in the phonebook`)
@@ -18,6 +20,10 @@ const App = () => {
 
   function handleNameChange(event: React.ChangeEvent<HTMLInputElement>) {
     setNewName(event.target.value)
+  }
+
+  function handleNumberChange(event: React.ChangeEvent<HTMLInputElement>) {
+    setNewNumber(event.target.value)
   }
 
   return (
@@ -28,13 +34,16 @@ const App = () => {
           name: <input value={newName} onChange={handleNameChange} />
         </div>
         <div>
+          number: <input value={newNumber} onChange={handleNumberChange} />
+        </div>
+        <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
       <ul>
         {
-          persons.map(person => <li key={person.name}>{person.name}</li>)
+          persons.map(({ name, number }) => <li key={name}>{name}, {number}</li>)
         }
       </ul>
     </div>
