9cfdc8c307b8508c96cac53deb2a6d3ee33b62ab
Janne Lavila <janne.lavila@wheelq.com>
2019-11-08 20:31:17 +0200

    fix types

--- osa5/blogi-frontend/types.d.ts
+++ osa5/blogi-frontend/types.d.ts
@@ -1,27 +1,27 @@
 export type User = {
-  token: string
-  username: string
+  token: string;
+  username: string;
 }
 
 export type Credentials = {
-  username: string
-  password: string
+  username: string;
+  password: string;
 }
 
 export type Message = {
-  content: string,
-  error?: boolean
+  content: string;
+  error?: boolean;
 }
 
 export type Blog = {
-  title: string
-  author: string
-  url: string
-  likes: number
-  id: string
+  title: string;
+  author: string;
+  url: string;
+  likes: number;
+  id: string;
   user: {
-    username: string
-  }
+    username: string;
+  };
 }
 
 export type NewBlog = Omit<Blog, 'likes' | 'id' | 'user'>
