dbb76783c8d268e27ad8beb661ec7a0ba1a53b0a
Janne Lavila <janne.lavila@wheelq.com>
2019-11-08 20:49:11 +0200

    implement 5.14

--- osa5/blogi-frontend/src/components/simpleBlog.test.tsx
+++ osa5/blogi-frontend/src/components/simpleBlog.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { render } from '@testing-library/react'
+import { render, fireEvent } from '@testing-library/react'
 import SimpleBlog from './SimpleBlog'
 import { Blog } from '../../types'
 
@@ -21,5 +21,13 @@ describe('<SimpleBlog />', () => {
     expect(queryByText(text => text.includes('kirjailija'))).toBeInTheDocument()
     expect(queryByText(text => text.includes('otsikko'))).toBeInTheDocument()
     expect(queryByText(text => text.includes('420'))).toBeInTheDocument()
+  })
+
+  test('should call onclick twice if bugtton is clicked twice', () => {
+    const mockFn = jest.fn()
+    const { getByText } = render(<SimpleBlog blog={blog} onClick={mockFn} />)
+    fireEvent.click(getByText('like'))
+    fireEvent.click(getByText('like'))
+    expect(mockFn).toHaveBeenCalledTimes(2)
   })
 })
