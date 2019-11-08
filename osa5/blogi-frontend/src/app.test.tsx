1699e28b3ec5c7d6da19236e563c89fda59939ee
Janne Lavila <janne.lavila@wheelq.com>
2019-11-08 22:17:40 +0200

    implement 5.16 & 5.17

---
+++ osa5/blogi-frontend/src/app.test.tsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import '@testing-library/jest-dom/extend-expect'
+
+import { render, waitForDomChange, RenderResult } from '@testing-library/react'
+
+jest.mock('./services/services')
+import App from './App'
+import { act } from 'react-dom/test-utils'
+
+describe('<App />', () => {
+  afterEach(localStorage.clear)
+
+  test('if no user logged, notes are not rendered', async () => {
+    const { queryByText } = render(<App />)
+    expect(queryByText('username')).toBeInTheDocument()
+    expect(queryByText('blogs')).not.toBeInTheDocument()
+  })
+
+  test('if no user logged, notes are not rendered', async () => {
+    localStorage.setItem('userName', 'testaaja')
+    localStorage.setItem('token', 'tokeni')
+    let component: RenderResult | undefined
+    await act(async () => {
+      component = render(<App />)
+      await waitForDomChange()
+    })
+
+    expect(component.queryByText('blogs for testaaja')).toBeInTheDocument()
+  })
+})
