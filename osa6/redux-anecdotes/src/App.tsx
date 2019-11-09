014a80333cf8ea62f7ebe499d700336c43c4c272
Janne Lavila <janne.lavila@wheelq.com>
2019-11-09 18:34:20 +0200

    implement 6.8

--- osa6/redux-anecdotes/src/App.tsx
+++ osa6/redux-anecdotes/src/App.tsx
@@ -1,29 +1,15 @@
 import React from 'react';
 import { StoreType } from '.';
-import { doVote } from './reducers/anecdoteReducer'
 import AnecdoteForm from './components/AnecdoteForm'
+import AnecdoteList from './components/AnecdoteList'
+
 
 const App = (props: { store: StoreType }) => {
-  const anecdotes = props.store.getState()
-
-  const vote = (id: string) => {
-    props.store.dispatch(doVote(id))
-  }
 
   return (
     <div>
       <h2>Anecdotes</h2>
-      {anecdotes.map(anecdote =>
-        <div key={anecdote.id}>
-          <div>
-            {anecdote.content}
-          </div>
-          <div>
-            has {anecdote.votes}
-            <button onClick={() => vote(anecdote.id)}>vote</button>
-          </div>
-        </div>
-      )}
+      <AnecdoteList store={props.store} />
       <AnecdoteForm store={props.store} />
     </div>
   )
---
+++ osa6/redux-anecdotes/src/components/AnecdoteList.tsx
@@ -0,0 +1,31 @@
+import React, { ReactElement } from 'react'
+import { doVote } from '../reducers/anecdoteReducer'
+import { StoreType } from '..'
+
+interface Props {
+  store: StoreType
+}
+
+export default function AnecdoteList({ store }: Props): ReactElement {
+  const anecdotes = store.getState()
+
+  const vote = (id: string) => {
+    store.dispatch(doVote(id))
+  }
+
+  return (
+    <>
+      {anecdotes.map(anecdote =>
+        <div key={anecdote.id}>
+          <div>
+            {anecdote.content}
+          </div>
+          <div>
+            has {anecdote.votes}
+            <button onClick={() => vote(anecdote.id)}>vote</button>
+          </div>
+        </div>
+      )}
+    </>
+  )
+}
