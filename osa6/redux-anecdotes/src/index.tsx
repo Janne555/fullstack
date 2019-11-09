32292ea96fe00037e67e7973dbd9c4de166d3f4d
Janne Lavila <janne.lavila@wheelq.com>
2019-11-09 20:19:29 +0200

    implement 6.16

--- osa6/redux-anecdotes/src/index.tsx
+++ osa6/redux-anecdotes/src/index.tsx
@@ -6,6 +6,8 @@ import anecdoteReducer from './reducers/anecdoteReducer'
 import notificationReducer from './reducers/notificationReducer'
 import filterReducer from './reducers/filterReducer'
 import { Provider } from 'react-redux'
+import axios from 'axios'
+import { Anecdote } from './types'
 
 const reducer = combineReducers({
   anecdotes: anecdoteReducer,
@@ -16,6 +18,12 @@ const reducer = combineReducers({
 const store = createStore(reducer)
 export type StoreType = typeof store
 export type StateType = ReturnType<typeof reducer>
+
+axios
+  .get<Anecdote[]>('http://localhost:3001/anecdotes')
+  .then(({ data }) => {
+    data.forEach(anecdote => store.dispatch({ type: "NEW", ...anecdote }))
+  })
 
 const render = () => {
   ReactDOM.render(
--- osa6/redux-anecdotes/src/reducers/anecdoteReducer.ts
+++ osa6/redux-anecdotes/src/reducers/anecdoteReducer.ts
@@ -1,31 +1,12 @@
 import { VoteAction, Anecdote, NewAnecdoteAction } from '../types'
 
-const anecdotesAtStart = [
-  'If it hurts, do it more often',
-  'Adding manpower to a late software project makes it later!',
-  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-  'Premature optimization is the root of all evil.',
-  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
-]
-
 export const getId = () => (100000 * Math.random()).toFixed(0)
-
-const asObject = (anecdote: string): Anecdote => {
-  return {
-    content: anecdote,
-    id: getId(),
-    votes: 0
-  }
-}
-
-const initialState = anecdotesAtStart.map(asObject)
 
 export const doCreate = (content: string): NewAnecdoteAction => ({ type: "NEW", id: getId(), content, votes: 0 })
 export const doVote = (id: string): VoteAction => ({ id, type: "VOTE" })
 
 
-const reducer = (state = initialState, action: VoteAction | NewAnecdoteAction) => {
+const reducer = (state: Anecdote[] = [], action: VoteAction | NewAnecdoteAction) => {
   let newState: Anecdote[] | undefined = undefined
   switch (action.type) {
     case 'VOTE':
