811955c28850a77dc051cca8c3461cda4974da0a
Janne Lavila <janne.lavila@wheelq.com>
2019-11-16 15:35:12 +0200

    implement 8.4

--- osa8/graphql-backend/dist/index.js
+++ osa8/graphql-backend/dist/index.js
@@ -101,7 +101,7 @@ const typeDefs = apollo_server_1.gql `
   type Query {
     bookCount: Int!
     authorCount: Int!
-    allBooks: [Book!]!
+    allBooks(author: String): [Book!]!
     allAuthors: [Author!]!
   }
 `;
@@ -109,7 +109,7 @@ const resolvers = {
     Query: {
         bookCount: () => books.length,
         authorCount: () => authors.length,
-        allBooks: () => books,
+        allBooks: (root, args) => books.filter(b => b.author === args.author),
         allAuthors: () => {
             return authors.map(a => ({ ...a, bookCount: books.filter(b => b.author === a.name).length }));
         }
